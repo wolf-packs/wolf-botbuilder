7403bb69530d5b3f73d49eb3364570f821a1e038
Kevin Leung <keleung@microsoft.com>
2019-02-07 14:54:54 -0600

    storage layer implementation

---
+++ src/storageLayer.ts
@@ -0,0 +1,48 @@
+import * as wolf from 'wolf-core'
+import { ConversationState, TurnContext } from 'botbuilder'
+
+interface AnyObject {
+  [key: string]: any,
+  [key: number]: any
+}
+
+/**
+ * Factory function that allows the creation of Wolf storage layer interfaces specific to
+ * Microsoft Botbuilder utilizing TurnContext
+ * 
+ * @param conversationState Botbuilder's ConversationState instance
+ * @param statePropertyName Optional state proprty name, default is 'CONVERSATION_STATE'
+ * @returns Wolf storage layer functions
+ */
+export const createBotbuilderStorageLayer = <T extends AnyObject>(
+  conversationState: ConversationState,
+  statePropertyName: string = 'CONVERSATION_STATE'
+) => {
+  const convoStore = conversationState.createProperty(statePropertyName)
+  return (botbuilderTurnContext: TurnContext, initialState?: T) => {
+    const convoStateStorage: wolf.StorageLayer<T> = {
+      read: async () => {
+        return await convoStore.get(botbuilderTurnContext, initialState)
+      },
+      save: async (newState: T) => {
+        // TODO: Why we chose to do this?
+        // remove old keys
+        const oldState = conversationState.get(botbuilderTurnContext)
+        const oldKeys = Object.keys(oldState)
+        oldKeys.forEach((key) => {
+          delete oldState[key]
+        })
+
+        // save new keys
+        const keys = Object.keys(newState)
+        keys.forEach((key) => {
+          oldState[key] = newState[key]
+        })
+
+        // save back to botbuilder context
+        await conversationState.saveChanges(botbuilderTurnContext)
+      }
+    }
+    return convoStateStorage
+  }
+}
